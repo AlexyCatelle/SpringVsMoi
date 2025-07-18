90c39395364b87a4b52cb79618c7f87a81ee5694
AlexyCatelle <alexy.catelle@oclock.school>
2025-07-18 14:18:33 +0200

    add UUID

--- src/main/java/org/example/demo_spring_base/model/Todo.java
+++ src/main/java/org/example/demo_spring_base/model/Todo.java
@@ -2,6 +2,8 @@ package org.example.demo_spring_base.model;
 
 import lombok.*;
 
+import java.util.UUID;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -9,6 +11,7 @@ import lombok.*;
 @Builder
 
 public class Todo {
+    private UUID id;
     private String name;
     private String description;
     private boolean isDone;
--- src/main/java/org/example/demo_spring_base/service/ToDoService.java
+++ src/main/java/org/example/demo_spring_base/service/ToDoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ToDoService {
@@ -12,9 +13,21 @@ public class ToDoService {
     private final List<Todo> todos = new ArrayList<>();
 
     public ToDoService() {
-        todos.add(new Todo("Acheter du pain", "Aller à la boulangerie", false));
-        todos.add(new Todo("Faire les devoirs", "Maths et physique", true));
-    }
+            todos.add(Todo.builder()
+                    .id(UUID.randomUUID())
+                    .name("Acheter du pain")
+                    .description("Aller à la boulangerie")
+                    .isDone(false)
+                    .build());
+
+            todos.add(Todo.builder()
+                    .id(UUID.randomUUID())
+                    .name("Faire les devoirs")
+                    .description("Maths et physique")
+                    .isDone(true)
+                    .build());
+        }
+
 
     public List<Todo> getAllTodos() {
         return todos;
