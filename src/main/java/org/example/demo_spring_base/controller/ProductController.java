fc561816e0dbf492f6861bfec36e00810e16cc17
AlexyCatelle <alexy.catelle@oclock.school>
2025-07-21 11:23:39 +0200

    thumleaf

--- src/main/java/org/example/demo_spring_base/controller/ProductController.java
+++ src/main/java/org/example/demo_spring_base/controller/ProductController.java
@@ -1,5 +1,6 @@
 package org.example.demo_spring_base.controller;
 
+import ch.qos.logback.core.model.Model;
 import org.example.demo_spring_base.model.Product;
 import org.example.demo_spring_base.service.ProductService;
 import org.springframework.stereotype.Controller;
@@ -9,36 +10,39 @@ import java.util.List;
 import java.util.UUID;
 
 @Controller
-@RequestMapping("/product")
 public class ProductController {
-private ProductService productService;
-
-public ProductController(ProductService productService) {
-    this.productService = productService;
-}
+//private ProductService productService;
+//
+//public ProductController(ProductService productService) {
+//    this.productService = productService;
+//}
 
 // All products
 @GetMapping("/all")
-@ResponseBody
-public List<Product> getAllProducts() {
-    return productService.getAllProducts();
+//@ResponseBody
+//public List<Product> getAllProducts() {
+//    return productService.getAllProducts();
+//}
+
+public String all() {
+    return "all";
 }
 // Par Id
-    @RequestMapping("/{id}")
-    @ResponseBody
-    public Product getProductById(@PathVariable UUID id) {
-    System.out.println("Mon id est : " + id);
-    return productService.getProductById(id);
-    }
+//    @RequestMapping("/{id}")
+//    @ResponseBody
+//    public Product getProductById(@PathVariable UUID id) {
+//    System.out.println("Mon id est : " + id);
+//    return productService.getProductById(id);
+//    }
 
 // Filtre
-    @GetMapping ("/filter")
-    @ResponseBody
-    public List<Product> filterProducts(
-            @RequestParam(required = false) String name,
-            @RequestParam(required = false) String category
-    ) {
-        return productService.filterProducts(name, category);
-    }
+//    @GetMapping ("/filter")
+//    @ResponseBody
+//    public List<Product> filterProducts(
+//            @RequestParam(required = false) String name,
+//            @RequestParam(required = false) String category
+//    ) {
+//        return productService.filterProducts(name, category);
+//    }
 
 }
